221e9236a33a826576c7c402fc304242467337a4
ziippy <ziippy@naver.com>
2022-04-02 18:15:59 +0900

    chapter3 - devtools & logger

---
+++ hacking-spring-boot-ch3-reactive/src/main/java/com/greglturnquist/hackingspringboot.reactive/CartItem.java
@@ -0,0 +1,26 @@
+package com.greglturnquist.hackingspringboot.reactive;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public class CartItem {
+    @NonNull private Item item;
+    private int quantity = 1;
+
+    private CartItem() {};
+
+    public CartItem(Item item) {
+        this.item = item;
+        this.quantity = 1;
+    }
+
+    public CartItem(Item item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public void increment() {
+        this.quantity++;
+    }
+}
---
+++ hacking-spring-boot-ch3-reactive/src/main/java/com/greglturnquist/hackingspringboot.reactive/ItemRepository.java
@@ -0,0 +1,27 @@
+package com.greglturnquist.hackingspringboot.reactive;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+
+public interface ItemRepository extends ReactiveCrudRepository<Item, String>,
+                                            ReactiveQueryByExampleExecutor<Item> {
+    Flux<Item> findByNameContaining(String partialName);
+
+//    @Query("{ 'name' : ?0, 'age' : ?1 }")
+//    Flux<Item> findItemsForCustomMonthlyReport(String name, int age);
+//
+//    @Query(sort = "{ 'age' : -1 }")
+//    Flux<Item> findSortedStuffForWeeklyReport();
+
+//    Flux<Item> findByDescription(String description);
+//    Flux<Item> findByDescriptionContaining(String partialDescription);
+    Flux<Item> findByDescriptionContainingIgnoreCase(String partialDescription);
+    Flux<Item> findByNameContainingAndDescriptionContaining(String partialName, String partialDescription);
+    Flux<Item> findByNameContainingOrDescriptionContaining(String partialName, String partialDescription);
+
+    // 요구사항에 따라 이렇게 점점 늘어날지도..
+
+    // 이를 대체하기 위해 Query by Example 을 이용해보자.
+}
